2372fac5a630c6fad9a42995c3d150c3223f62f4
Rifqi Akmal <rifqiakmalf@gmail.com>
2023-04-16 11:45:45 +0700

    Login & Register

---
+++ src/Register.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+                <h1>REGISTER</h1>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Username</label>
+                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Username" />
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@example.com" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Register</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
+        </div>
+    )
+}
